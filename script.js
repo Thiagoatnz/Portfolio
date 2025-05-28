1d95ffc034cad649b2560b5addab58fdb0123cbd
Thiago Atnz <thiagoatnz@gmail.com>
2025-05-28 13:05:59 -0300

    Subindo tudo do zero, versão final

--- script.js
+++ script.js
@@ -33,3 +33,20 @@ btnTop.addEventListener('click', function() {
         behavior: 'smooth'
     });
 });
+
+const btnTop = document.getElementById("btnTop");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+});
+
+btnTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
