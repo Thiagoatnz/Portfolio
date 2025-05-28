0acc2d6dd0ae80677e3189c2ce0b9bd6a172bf1b
Thiago Atnz <thiagoatnz@gmail.com>
2025-05-28 12:52:31 -0300

    Subindo tudo do zero, versão final

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+// Dark/Light Mode com Local Storage
 const toggle = document.querySelector('.toggle-mode');
 const body = document.querySelector('body');
 
+// Checa se tem modo salvo
+if(localStorage.getItem('mode') === 'dark') {
+    body.classList.add('dark');
+}
+
 toggle.addEventListener('click', () => {
     body.classList.toggle('dark');
+    if(body.classList.contains('dark')) {
+        localStorage.setItem('mode', 'dark');
+    } else {
+        localStorage.setItem('mode', 'light');
+    }
+});
+
+// Botão Voltar ao Topo
+const btnTop = document.getElementById('btnTop');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+};
+
+btnTop.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
